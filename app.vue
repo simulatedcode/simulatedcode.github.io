b17e9954dd5b1d5d63caf7ddd736c2964783da1f
simulatedcode <simulasi.studio@gmail.com>
2023-04-25 11:39:42 +0700

    add(tailwind):css

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    hello
+  <div class="p-1 dark:bg-neutral-800 h-screen">
+    <p class="text-sm text-gray-500">simulatedcode</p>
+    <p class="text-sm text-gray-500">code: <span class="mt-4 animate-pulse delay-300 text-lg dark:text-green-500">|</span>
+    </p>
   </div>
 </template>
