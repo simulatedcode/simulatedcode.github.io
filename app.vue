a807f2baf8aaf154d356278b4fbdb563de3acfda
simulatedcode <simulasi.studio@gmail.com>
2023-04-25 12:18:13 +0700

    edit(app):title

--- app.vue
+++ app.vue
@@ -1,4 +1,8 @@
 <template>
+  <HEAD>
+    <title>Simulatedcode</title>
+    <meta name="description" description="simulated" />
+  </HEAD>
   <div class="p-1 dark:bg-neutral-800 h-screen">
     <p class="text-sm text-gray-500">simulatedcode</p>
     <p class="text-sm text-gray-500">code: <span class="mt-4 animate-pulse delay-300 text-lg dark:text-green-500">|</span>
